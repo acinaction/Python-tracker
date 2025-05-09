4921aa1ce5c707aa5f44c4fdef4cb708c82e8309
acinaction <chauhanarsh1312@gmail.com>
2025-05-10 00:26:31 +0530

    Data types and their usage

---
+++ basics/DataTypes.py
@@ -0,0 +1,10 @@
+#Here are the data-types in Python;
+#Creating a variable ->
+character_name = "John" #String
+character_age = 35 #Number
+character_is_male = True #Boolean
+#Printing the variable ->
+print("There once was a man named " + character_name + ",")
+print("he was " + str(character_age) + " years old.")
+print("He really liked the name " + character_name + ",")
+print("but didn't like being " + str(character_age) + ".")
