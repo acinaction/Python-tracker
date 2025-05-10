6f54c198b9ac9b459a8d0199a1623ba91f60caf1
acinaction <chauhanarsh1312@gmail.com>
2025-05-10 15:59:02 +0530

    Learned the basics of strings before string methods

---
+++ Strings/basics.py
@@ -0,0 +1,9 @@
+# Creating new line \n;
+# Creating tab \t;
+# Creating backslash \\ or \;
+# Creating a quotation mark \";
+phrase = "Giraffe Academy"
+# Concatenating strings;
+print(phrase + " is cool")
+# Getting the length of a string;
+print(len(phrase))
