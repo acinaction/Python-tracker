ec1c0f3fc940ec92f1669a09b6676f62b4a566fb
acinaction <chauhanarsh1312@gmail.com>
2025-05-10 16:12:45 +0530

    Basics of operators

---
+++ Numbers/Basics.py
@@ -0,0 +1,31 @@
+# The operators 
+# +, -, *, /, //, %, ** are used for addition, subtraction, multiplication,
+# division, floor division, modulus and exponentiation respectively.
+a=10
+b=3
+print(a+b) # Addition
+print(a-b) # Subtraction
+print(a*b) # Multiplication
+print(a/b) # Division
+print(a//b) # Floor Division
+print(a%b) # Modulus
+print(a**b) # Exponentiation
+# The comparison operators
+# ==, !=, >, <, >=, <= are used for equality, inequality, greater than,
+# less than, greater than or equal to and less than or equal to respectively.
+print(a==b) # Equality
+print(a!=b) # Inequality
+print(a>b) # Greater than
+print(a<b) # Less than
+print(a>=b) # Greater than or equal to
+print(a<=b) # Less than or equal to
+# The logical operators
+# and, or, not are used for logical AND, logical OR and logical NOT respectively.
+print(a>5 and b<5) # Logical AND
+print(a>5 or b<5) # Logical OR
+print(not(a>5)) # Logical NOT
+# The assignment operators
+# =, +=, -=, *=, /=, //=, %=, **= are used for assignment, addition assignment,
+# subtraction assignment, multiplication assignment, division assignment,
+# floor division assignment, modulus assignment and exponentiation assignment
+# respectively.
