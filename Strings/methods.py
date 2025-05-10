8d013bf69c168b352ad56d50213406ec1be327c5
acinaction <chauhanarsh1312@gmail.com>
2025-05-10 16:05:30 +0530

    Some of the methods and functions in strings in python

---
+++ Strings/methods.py
@@ -0,0 +1,12 @@
+phrase="Giraffe Academy"
+print(phrase.lower()) # Converts the string to lowercase
+print(phrase.upper()) # Converts the string to uppercase
+print(phrase.isupper()) # Checks if the string is in uppercase
+print(phrase.islower()) # Checks if the string is in lowercase
+print(phrase.upper().isupper()) # Converts the string to uppercase and then checks if it is in uppercase 
+# Indexing 
+print(phrase[0]) # Prints the first character of the string
+# Index function
+print(phrase.index("G")) # Prints the index of the first occurrence of "G"
+# replace function
+print(phrase.replace("Giraffe", "Elephant")) # Replaces "Giraffe" with "Elephant"
